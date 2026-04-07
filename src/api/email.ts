356a16bd22ef340c588662b643252c7deae0c01e
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2026-04-07 13:31:45 +0300

    deploy render

--- src/api/email.ts
+++ src/api/email.ts
@@ -1,12 +1,12 @@
 export const sendReport = async (email: string, reportHTML: string) => {
-  const res = await fetch('http://localhost:5001/send-report', {
+  const res = await fetch('https://habits-tracker-app-j1tr.onrender.com/send-report', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       email,
-      reportHTML,   
+      reportHTML,
     }),
   })
 
