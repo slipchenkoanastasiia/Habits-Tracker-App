daa713f87f95704ae129e7b627b4d5f6c4c24324
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2026-04-08 09:35:47 +0300

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
-  base: '/Habits-Tracker-App/', 
+// export default з динамічним base
+export default defineConfig(({ mode }) => ({
+  // Якщо збірка для продакшн (npm run build), ставимо підпапку GitHub Pages
+  // Локально (npm run dev) — просто '/'
+  base: mode === 'production' ? '/Habits-Tracker-App/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-    extensions: ['.js', '.ts', '.vue']
+    extensions: ['.js', '.ts', '.vue'],
   },
   server: {
-    watch: {
-      usePolling: true,
-    },
+    watch: { usePolling: true },
   },
-})
+}))
